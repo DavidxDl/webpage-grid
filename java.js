3f345cea3d1a7aeb9df985df72ffb9904684eade
DavidxDl <Davidbekamlopez@gmail.com>
2022-09-12 21:30:14 -0300

    idk what the fuck i did, but fuck event delegations

--- java.js
+++ java.js
@@ -1,11 +1,35 @@
 const container = document.querySelector('.container');
-for(let i = 0; i < 256; i++){
-    let square = document.createElement('div');
-    square.classList = 'square';
+let btn = document.querySelector('#btn');
+let size = 256;
 
-    container.appendChild(square);
-}
-let hover = document.querySelectorAll('.square');
+btn.addEventListener('click', () => {
+    size = prompt('What size do u want?') ** 2;
+
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+ }
+
+    for(let i = 0; i < size; i++){
+        let newSquare = document.createElement('div');
+        newSquare.classList = 'square';
+        container.appendChild(newSquare);
+        
+    };
+    
+})
 
 
-hover.forEach(square => square.addEventListener('mouseover', (e) => {square.classList.add('hover')}))
+
+
+for(let i = 0; i < size; i++){
+    let newSquare = document.createElement('div');
+    newSquare.classList = 'square';
+    container.appendChild(newSquare);
+    
+};
+
+container.addEventListener('mouseover', (e) => {
+    if (e.target && e.target.classList.contains('square'))
+    {let target = e.target;
+      target.classList.add('hover')          }
+})
